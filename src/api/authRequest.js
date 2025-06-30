d8a595f8d4cbe769c7d92ad88fbacd61cbe04bbb
closeLIDER995 <abduvaliyevabdulloh881@>
2025-06-30 09:09:33 +0300

    fix: updated BASE_URL

--- src/api/authRequest.js
+++ src/api/authRequest.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_SERVER_URL || 'https://imtihon-backend-vu9t.onrender.com/api';
+const BASE_URL = 'https://imtihon-backend-vu9t.onrender.com'; // <-- .env dan olib kelishingiz ham mumkin
 
 const API = axios.create({ baseURL: BASE_URL });
 
-export const register = (formData) => API.post('/signup', formData);
-export const login = (formData) => API.post('/login', formData);   
+export const register = (formData) => API.post('/api/signup', formData);
+export const login = (formData) => API.post('/api/login', formData);
